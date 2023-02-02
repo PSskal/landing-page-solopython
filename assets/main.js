f474ebca13fbd23bb17558034d0a628c7a55033e
Richard Pascual <ricpascual@gmail.com>
2023-02-02 12:31:51 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -23,9 +23,10 @@ const fetchData = async (urlApi) => {
         let view = `
         ${videos.items.map(video => `
         <div class="group relative">
+        <a href=${video.snippet.thumbnails.high.url} >
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          <img src=${video.snippet.thumbnails.high.url} alt="${video.snippet.description}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
           <h3 class="text-sm text-gray-700">
@@ -33,6 +34,7 @@ const fetchData = async (urlApi) => {
             ${video.snippet.title}
           </h3>
         </div>
+        </a>
       </div>
 
         `).slice(0,8).join("")}  
